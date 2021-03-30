687cdd1882b224e5c4a012b94d498edee4c92d31
victoria-lokteva <vilokteva@yandex.ru>
2021-03-30 13:46:33 +0300

    commit

---
+++ experiment.py
@@ -0,0 +1,18 @@
+import torch
+import torchvision.transforms as transforms
+from training import  random_seed, Dataset, train
+from alert import Alert
+
+random_seed(10)
+
+transforms = transforms.Compose([transforms.Resize((48, 48)), transforms.ToTensor()])
+
+train_data = Dataset(filename_img, filename_labels, transforms)
+test_data = Dataset(ilename_img, filename_labels, transforms)
+
+data_loader = torch.utils.data.DataLoader(train_data, batch_size)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size)
+
+net = Alert()
+
+train(net)
---
+++ training.py
@@ -0,0 +1,73 @@
+import numpy as np
+import torch
+from PIL import Image
+
+
+def random_seed(rs=10):
+    np.random.seed(rs)
+    torch.manual_seed(rs)
+    torch.cuda.manual_seed(rs)
+    torch.backends.cudnn.deterministic = True
+
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, images_file, labels_file, transforms):
+        super().__init__()
+        self.images_file = images_file
+        self.labels_file = labels_file
+        self.transform = transforms
+        with open(self.images_file, 'r') as f:
+            self.roots = f.readlines()
+            self.roots = [root[:-1] for root in self.roots]
+        with open(self.labels_file, 'r') as f:
+            self.labels = f.readlines()
+            self.labels = [label[:-1] for label in self.labels]
+            self.labels = [float(label) for label in self.labels]
+
+    def __iter__(self, x):
+        return iter(x)
+
+    def __len__(self):
+        return len(self.roots)
+
+    def __getitem__(self, index):
+        root = self.roots[index]
+        label = self.labels[index]
+        image = Image.open(root)
+        image = self.transform(image)
+        return image, label
+
+
+def test(net):
+    predictions = []
+    test_loader = iter(test_loader)
+    with torch.no_grad():
+        for idx, image in enumerate(test_loader):
+            image = image.to(device)
+            pred = net.forward(image)
+            predictions.append(pred)
+    return predictions
+
+def train(net, lr=0.001, num_epoch=20):
+
+    device = torch.device('cuda') if torch.cuda.is_available else torch.device('cpu')
+    loss_func = torch.nn.BCELoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=4)
+    train_loader = iter(data_loader)
+    net = net.to(device)
+
+    for epoch in range(num_epoch):
+        for idx, (image, label) in enumerate(train_loader):
+            image = image.to(device)
+            label = label.to(device)
+            optimizer.zero_grad()
+            outputs = net(image)
+            loss = loss_func(outputs, label)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+        test(net)
+    return net
