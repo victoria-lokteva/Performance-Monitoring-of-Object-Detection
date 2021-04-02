a9cb692574d9ca602cd63584de8790c5a6027014
victoria-lokteva <vilokteva@yandex.ru>
2021-04-02 19:28:01 +0300

    commit

--- experiment.py
+++ experiment.py
@@ -1,21 +1,25 @@
 import torch
 import torchvision.transforms as transforms
 import wandb
+import yaml
 from training import  random_seed, Dataset, train
 from alert import Alert
 
 random_seed(10)
 
+config = open('configs.yaml')
+congig = yaml.load(config, loader=yaml.FullLoader)
+
 wandb.init(name='training', project='alert', entity='')
 
 transform = transforms.Compose([transforms.Resize((48, 48)), transforms.ToTensor()])
 
-train_data = Dataset('filename_img', 'filename_labels', transform)
-test_data = Dataset('test_filename_img', 'test_filename_labels', transform)
+train_data = Dataset(filename_img, filename_labels, transform)
+test_data = Dataset(test_filename_img, test_filename_labels, transform)
 
-data_loader = torch.utils.data.DataLoader(train_data, 'batch_size')
-test_loader = torch.utils.data.DataLoader(test_data, 'batch_size')
+data_loader = torch.utils.data.DataLoader(train_data, batch_size)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size)
 
-net = Alert('initialization')
+net = Alert(initialization)
 
 train(net, data_loader, test_loader)
